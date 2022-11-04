188ef5712a879ebf6b06d43bf62aa358a92dca8f
Daphineatim <hopedaphine19@gmail.com>
2022-11-04 20:33:15 +0300

    Fix linters

--- src/components/Books.js
+++ src/components/Books.js
@@ -2,28 +2,30 @@ import React from 'react';
 import NewBook from './NewBook';
 import AddBook from './AddBook';
 
-const Books = () => (
-  <>
-    <NewBook
-      title="Hunger Games"
-      author="Suzanne Collins"
-      status="64%"
-      chapter="chapter 17"
-    />
-    <NewBook
-      title="Hunger Games"
-      author="Suzanne Collins"
-      status="64%"
-      chapter="chapter 17"
-    />
-    <NewBook
-      title="Hunger Games"
-      author="Suzanne Collins"
-      status="64%"
-      chapter="chapter 17"
-    />
-    <AddBook />
-  </>
-);
+function Books() {
+  return (
+    <>
+      <NewBook
+        title="Hunger Games"
+        author="Suzanne Collins"
+        status="64%"
+        chapter="chapter 17"
+      />
+      <NewBook
+        title="Hunger Games"
+        author="Suzanne Collins"
+        status="64%"
+        chapter="chapter 17"
+      />
+      <NewBook
+        title="Hunger Games"
+        author="Suzanne Collins"
+        status="64%"
+        chapter="chapter 17"
+      />
+      <AddBook />
+    </>
+  );
+}
 
 export default Books;
