c74260c283ac78445547a4408d6f31f45752e4d1
Daphineatim <hopedaphine19@gmail.com>
2022-11-09 15:35:14 +0300

    Add changes to Categories.js to handle click on button, Update AddBooks.js and Books.js

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { checkStatusAction } from '../redux/categories/categories';
 
-function Categories() {
+const Categories = () => {
+  const status = useSelector((state) => state.categoriesReducer);
+  const dispatch = useDispatch();
+
+  const updateCategories = (event) => {
+    event.preventDefault();
+
+    dispatch(checkStatusAction());
+  };
+
   return (
-    <div className="container p-0">
-      <button type="button" className="btn">
-        Check Status
-      </button>
-    </div>
+    <>
+      <div className="container p-0">
+        <button type="button" className="btn" onClick={updateCategories}>
+          Check Status
+        </button>
+        <h4 className="pt-5 text-5">{status}</h4>
+      </div>
+    </>
   );
-}
+};
+
 export default Categories;
